4cf4fc0b34be9a9f6042796c6eeb0b2014e32997
Михаил Волков <mgvolkov25@gmail.com>
2021-11-20 21:46:29 +0300

    added fanagoriyskaya cave tour

---
+++ fanagoriyskaya-cave/update_xml_tour.py
@@ -0,0 +1,42 @@
+import os
+
+path = "C:\\Users\\Michael\\Documents\\GitHub\\panoramas\\fanagoriyskaya-cave\\"
+mainfilename = "fanagor-cave.xml"
+
+l = []
+
+for file in os.listdir(path):
+    if "xml" in file:
+        l.append(file)
+
+mainfile = open(path + mainfilename, "r", encoding='utf-8')
+mainlines = mainfile.readlines()
+
+table = {}
+with open(path + "keys.txt") as f:
+    for line in f:
+        k, v = line.split()
+        table[k] = v
+
+for key in table:
+    file = open(path + key + ".xml", "w", encoding='utf-8')
+    for line in mainlines:
+        if "tour start" in line:
+            line = "<tour start=\"%s\">\n" % table[key]
+        file.write(line)
+    file.close()
+
+
+# generate html files:
+mainfilename = "fanagor-cave.html"
+mainfile = open(path + mainfilename, "r", encoding='utf-8')
+mainlines = mainfile.readlines()
+mainfile.close()
+
+for key in table:
+    file = open(path + key + ".html", "w", encoding='utf-8')
+    for line in mainlines:
+        if "pano.readConfigUrl" in line:
+            line = "\t\t\tpano.readConfigUrl(\"%s.xml\");\n" % key
+        file.write(line)
+    file.close()
